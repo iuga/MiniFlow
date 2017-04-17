07960e7910697b02bddce8fe64d152b0782301a6
Iuga <iugamovil@gmail.com>
2017-04-17 19:09:47 -0300

    Backprop and SDG Update information

--- miniflow/engine.py
+++ miniflow/engine.py
@@ -61,6 +61,31 @@ class Engine(object):
     def forward_and_backward(self, graph):
         """
         Performs a forward pass and a backward pass through a list of sorted Nodes.
+        Every node in the network get some inputs and can right awas compute two things:
+        - The output value: forward()
+        - The local gradient of its inputs with respect to its ouput value: backward()
+        Notice that the layers can do this completely independently without being aware of
+        any of the details of the full network that they are part. However, once the forward
+        pass is over, during backpropagation the layer will learn about the gradient of it's
+        ouput value on the final output of the network. The chain rule says that the layer
+        should take the gradient and multiply it into every gradient it normally computes for
+        all of its inputs.
+
+        Chain Rule
+        ----------
+        This expresion:
+            f(x, y, z) = (x + y) * z
+        can be broken down into two expresions:
+            - q = (x + y)
+            - f = q * z
+        The chain rule tells us that the correct way to "chain" these gradients expressions
+        together is through multiplication. It could be visualized through nodes:
+
+            [x]+-+
+                 +->[+]+
+            [y]+-+     |
+                       +->[*]+> = (x+y)*z
+            [z]+-------+
 
         Arguments:
             `graph`: The result of calling `topological_sort`.
@@ -68,26 +93,35 @@ class Engine(object):
         # Forward pass
         for n in graph:
             n.forward()
-
         # Backward pass
-        # see: https://docs.python.org/2.3/whatsnew/section-slices.html
         for n in graph[::-1]:
             n.backward()
 
     def sgd_update(self, trainables, learning_rate=1e-2):
         """
-        Updates the value of each trainable with SGD.
+        Process of finding the set of parameters W,b,etc that minimize the loss function.
+        The gradients tell us the direction in which the function has the steepest rate of
+        increase, but it does not tell us how far along this direction should be.
+        Choosing the step size ( learining rate ) will become one of the most important
+        hyperaparameters settings in training a neural network.
+
+        This method updates the value of each trainable with Stochastic Gradient Descent.
+
+        The gradient of a mini-batch is a good aproximation of the gradient of the full objective.
+
+        Vanilla Gradient Descent:
+        This simple loop is the core of all Neural Network libraries:
+        >>> while True:
+        >>>     # new_params -= size * direction
+        >>>     weights += - step_size * weights_gradient
 
         Arguments:
             `trainables`: A list of `Input` Nodes representing weights/biases.
             `learning_rate`: The learning rate.
         """
-        # Performs SGD
-        #
         # Loop over the trainables
-        for t in trainables:
+        for trainable in trainables:
             # Change the trainable's value by subtracting the learning rate
-            # multiplied by the partial of the cost with respect to this
-            # trainable.
-            partial = t.gradients[t]
-            t.value -= learning_rate * partial
+            # multiplied by the partial of the cost with respect to this trainable.
+            partial = trainable.gradients[trainable]
+            trainable.value -= learning_rate * partial
--- miniflow/topology.py
+++ miniflow/topology.py
@@ -8,7 +8,7 @@ from sys import stdout
 
 class Model(object):
     """
-    Define a generic Network model topology
+    Define a generic Network model topology and provide methods to train the Network
     """
     losses = {
         'mse': MSE
@@ -32,11 +32,33 @@ class Model(object):
         self.loss = self.losses.get(loss, MSE)
 
     def train(self, X_train, y_train, feed_dict={}, X_test=None, y_test=None, epochs=1000, batch_size=128, learning_rate=0.01):
-        # Total number of examples
+        """
+        Try to reach the bottom of the loss function as a high-dimensional optimization landscape:
+
+        We motivated the idea of optimizing the loss function with iterative refinement, where we start
+        with a random set of weights and refine them step by step until the loss is minimized.
+
+        This method performs a Mini-Batch Gradient Descent:
+        In large-scale applications the training data can have millions of examples, Hence, it seems wasteful
+        to compute the full loss function over the entire training set in order to perform a single parameter
+        update. A very common approach to addressing this challenge is to compute the gradient over batches
+        of training data.
+
+        Parameters:
+            `X_train`<List>: The data used to create the minibatches
+            `y_train`<List>: The correct labels for each example in X_train
+            `feed_dict`<Dict>: Dictionary with the placeholders and the data to use in them.
+            `X_test`<List>: The data used to validate the minibatches
+            `y_test`<List>: The correct labels for each example in X_test
+            `epochs`<int>: The numbers of epochs
+            `batch_size`<int>: The size of the mini-batch used to train
+            `learning_rate`<float>: The step size to update the trainables.
+        """
+        # Calculate the number of mini-batches per epoch:
         steps_per_epoch = X_train.shape[0] // batch_size
-        # Sort the graph
+        # Sort the network graph
         self.graph = self.engine.topological_sort(feed_dict)
-        # Network Inputs:
+        # Crete the Network Inputs:
         y_input_value = Input(trainable=False, name="y_input")
         # Add the loss layer in the graph:
         loss_layer = self.loss(y_input_value)(self.outputs)
@@ -56,11 +78,10 @@ class Model(object):
             loss = 0
             test_loss = 0
             with progressbar(range(steps_per_epoch), label='Epoch {}: '.format(i + 1), file=stdout) as bar:
-                for j in bar:
+                for mini_batch in bar:
                     # Step 1
-                    # Randomly sample a batch of examples
+                    # Randomly sample a batch of examples:
                     X_batch, y_batch = resample(X_train, y_train, n_samples=batch_size)
-
                     # Reset value of X and y Inputs
                     for input in self.inputs:
                         input.value = X_batch
